8114e5aef0e4bc21f0cb5cdd0458f6ea2b71f0e3
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-10-06 17:09:46 +0100

    Adding authentication tokens
    - configured in the config file
    - local build has one valid token - used in tests and for development
    - overridden with a private one in live
    
    Responses:
    - 401 for no token
    - 403 for invalid token
    
    Also adds caching headers - all responses with a cache header of 24 hours.

---
+++ tests/test_helpers.py
@@ -0,0 +1,32 @@
+from app import create_app
+import os
+
+
+class WSGIApplicationWithEnvironment(object):
+    def __init__(self, app, **kwargs):
+        self.app = app
+        self.kwargs = kwargs
+
+    def __call__(self, environ, start_response):
+        for key, value in self.kwargs.items():
+            environ[key] = value
+        return self.app(environ, start_response)
+
+
+class BaseApiTest(object):
+
+    def setup(self):
+        self.app = create_app('test')
+        self.client = self.app.test_client()
+        self.setup_authorization()
+
+    def setup_authorization(self):
+        """Set up bearer token and pass on all requests"""
+        valid_token = 'valid-token'
+        self.app.wsgi_app = WSGIApplicationWithEnvironment(
+            self.app.wsgi_app,
+            HTTP_AUTHORIZATION='Bearer {}'.format(valid_token))
+        self._auth_tokens = os.environ.get('DM_API_AUTH_TOKENS')
+
+    def do_not_provide_access_token(self):
+        self.app.wsgi_app = self.app.wsgi_app.app
