1fc5f351f5f05ce51fcfa9406d194ef32b8aa167
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-10-06 13:32:19 +0100

    Some debugging on request headers

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -12,6 +12,7 @@ def check_url_scheme():
     if current_app.config.get('NOTIFY_API_ENVIRONMENT', 'development') == 'live':
         scheme = request.headers.get('werkzeug.proxy_fix.orig_wsgi_url_scheme', 'http')
         preferred_scheme = current_app.config.get('NOTIFY_HTTP_PROTO', 'http')
+        print(reques.headers)
         if scheme != preferred_scheme:
             return redirect(request.url.replace('http://', 'https://', 1), code=301)
 
