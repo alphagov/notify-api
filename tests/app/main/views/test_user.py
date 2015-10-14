5e5bb355529642d23baa627a6d0f2b76e37ca24f
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-10-14 15:42:50 +0100

    Added user auth endpoints
    - take an email and password and compare with database
    - will keep track of failed logins
    - users with more than 5 failed logins will be locked
    - reset via successful login (can't unlock a locked account though)

--- tests/app/main/views/test_user.py
+++ tests/app/main/views/test_user.py
@@ -1,68 +1,189 @@
 from flask import json
 
 
-def test_should_be_able_to_get_job_by_id(notify_api, notify_db, notify_db_session, notify_config):
-    response = notify_api.test_client().get('/job/1234')
-    data = json.loads(response.get_data())
-    assert response.status_code == 200
-    assert data['job']['id'] == 1234
-    assert data['job']['name'] == 'job test'
-
-
-def test_should_be_a_404_if_job_does_not_exist(notify_api):
-    response = notify_api.test_client().get('/job/12345')
-    assert response.status_code == 404
-
-
-def test_should_be_a_404_if_job_id_is_not_an_int(notify_api):
-    response = notify_api.test_client().get('/job/invalid-id')
-    assert response.status_code == 404
-
-
-def test_should_be_able_to_create_a_job(notify_api, notify_db_session, notify_config):
-    response = notify_api.test_client().post(
-        '/job',
-        data=json.dumps(
-            {
-                'job': {
-                    'serviceId': 1234,
-                    'name': 'my job'
-                }
-            }
-        ),
-        content_type='application/json')
-    data = json.loads(response.get_data())
-    assert response.status_code == 201
-    assert 'job' in data
-    assert 'id' in data['job']
-
-
-def test_should_reject_an_invalid_job(notify_api, notify_config):
-    response = notify_api.test_client().post(
-        '/job',
-        data=json.dumps(
-            {
-                'job': {
-                    'serviceId': 'not-valid',
-                    'name': '1'
-                }
-            }
-        ),
-        content_type='application/json')
+def test_should_reject_fetch_user_request_with_no_email(notify_api, notify_db, notify_db_session):
+    response = notify_api.test_client().get('/users')
     data = json.loads(response.get_data())
     assert response.status_code == 400
-    assert data['error'] == 'Invalid JSON'
-    assert len(data['error_details']) == 2
-    assert {'key': 'serviceId',  'message': "'not-valid' is not of type 'integer'"} in data['error_details']
-    assert {'key': 'name',  'message': "'1' is too short"} in data['error_details']
+    assert data['error'] == 'No email address provided'
 
 
-def test_should_reject_if_no_job_root_element(notify_api, notify_config):
+def test_should_fetch_user_if_can_find_by_email(notify_api, notify_db, notify_db_session):
+    response = notify_api.test_client().get('/users?email_address=test-user@example.org')
+    data = json.loads(response.get_data())
+    assert response.status_code == 200
+    assert 'users' in data
+    assert data['users']['organisationId'] == 1234
+    assert data['users']['role'] == 'admin'
+    assert data['users']['emailAddress'] == 'test-user@example.org'
+    assert not data['users']['locked']
+    assert data['users']['active']
+    assert data['users']['failedLoginCount'] == 0
+
+
+def test_should_only_allow_valid_auth_requests(notify_api, notify_db, notify_db_session):
+    response = notify_api.test_client().post(
+        '/users/auth',
+        data=json.dumps(
+            {
+                'userAuthentication': {
+                    'password': 'valid-password'
+                }
+            }
+        ),
+        content_type='application/json')
+    data = json.loads(response.get_data())
+    print(data)
+    assert response.status_code == 400
+    assert 'error_details' in data
+    assert {'required': ["'emailAddress' is a required property"]} in data['error_details']
+
+
+def test_should_not_show_password_in_error_responses(notify_api, notify_db, notify_db_session):
+    response = notify_api.test_client().post(
+        '/users/auth',
+        data=json.dumps(
+            {
+                'userAuthentication': {
+                    'emailAddress': 'test-user@example.org',
+                    'password': 'v'
+                }
+            }
+        ),
+        content_type='application/json')
+    data = json.loads(response.get_data())
+    print(data)
+    assert response.status_code == 400
+    assert 'error_details' in data
+    assert {'key': 'password', 'message': 'Invalid password'} in data['error_details']
+
+
+def test_should_be_able_to_auth_user(notify_api, notify_db, notify_db_session):
+    response = notify_api.test_client().post(
+        '/users/auth',
+        data=json.dumps(
+            {
+                'userAuthentication': {
+                    'emailAddress': 'test-user@example.org',
+                    'password': 'valid-password'
+                }
+            }
+        ),
+        content_type='application/json')
+    data = json.loads(response.get_data())
+    print(data)
+    assert response.status_code == 200
+    assert 'users' in data
+    assert data['users']['organisationId'] == 1234
+    assert data['users']['role'] == 'admin'
+    assert data['users']['emailAddress'] == 'test-user@example.org'
+    assert not data['users']['locked']
+    assert data['users']['active']
+
+
+def test_should_404_if_user_not_found_on_auth(notify_api, notify_db, notify_db_session):
+    response = notify_api.test_client().post(
+        '/users/auth',
+        data=json.dumps(
+            {
+                'userAuthentication': {
+                    'emailAddress': 'test-user123@example.org',
+                    'password': 'valid-password'
+                }
+            }
+        ),
+        content_type='application/json')
+    assert response.status_code == 404
+
+
+def test_should_increment_failed_login_count(notify_api, notify_db, notify_db_session):
+    response = notify_api.test_client().post(
+        '/users/auth',
+        data=json.dumps(
+            {
+                'userAuthentication': {
+                    'emailAddress': 'test-user@example.org',
+                    'password': 'invalid-password'
+                }
+            }
+        ),
+        content_type='application/json')
+    assert response.status_code == 403
+
+    fetch_response = notify_api.test_client().get('/users?email_address=test-user@example.org')
+    data = json.loads(fetch_response.get_data())
+    assert fetch_response.status_code == 200
+    assert data['users']['failedLoginCount'] == 1
+
+
+def test_should_reset_failed_login_count_on_success(notify_api, notify_db, notify_db_session):
+    response_1 = notify_api.test_client().post(
+        '/users/auth',
+        data=json.dumps(
+            {
+                'userAuthentication': {
+                    'emailAddress': 'test-user@example.org',
+                    'password': 'invalid-password'
+                }
+            }
+        ),
+        content_type='application/json')
+    assert response_1.status_code == 403
+
+    fetch_response_1 = notify_api.test_client().get('/users?email_address=test-user@example.org')
+    data = json.loads(fetch_response_1.get_data())
+    assert fetch_response_1.status_code == 200
+    assert data['users']['failedLoginCount'] == 1
+
+    response_2 = notify_api.test_client().post(
+        '/users/auth',
+        data=json.dumps(
+            {
+                'userAuthentication': {
+                    'emailAddress': 'test-user@example.org',
+                    'password': 'valid-password'
+                }
+            }
+        ),
+        content_type='application/json')
+    assert response_2.status_code == 200
+
+    fetch_response_2 = notify_api.test_client().get('/users?email_address=test-user@example.org')
+    data = json.loads(fetch_response_2.get_data())
+    assert fetch_response_2.status_code == 200
+    assert data['users']['failedLoginCount'] == 0
+
+
+def test_shouyld_prevent_login_when_too_many_failed_attempts(notify_api, notify_db, notify_db_session):
+    for i in range(0, notify_api.config['MAX_FAILED_LOGIN_COUNT'] + 1):
         response = notify_api.test_client().post(
-            '/job',
-            data=json.dumps({}),
-            content_type='application/json'
-        )
-        data = json.loads(response.get_data())
-        assert data['error'] == "Invalid JSON; must have job as root element"
-        assert response.status_code == 400
+            '/users/auth',
+            data=json.dumps(
+                {
+                    'userAuthentication': {
+                        'emailAddress': 'test-user@example.org',
+                        'password': 'invalid-password'
+                    }
+                }
+            ),
+            content_type='application/json')
+        assert response.status_code == 403
+
+    fetch_response = notify_api.test_client().get('/users?email_address=test-user@example.org')
+    data = json.loads(fetch_response.get_data())
+    assert fetch_response.status_code == 200
+    assert data['users']['failedLoginCount'] == 6
+    assert data['users']['locked']
+
+    response_2 = notify_api.test_client().post(
+        '/users/auth',
+        data=json.dumps(
+            {
+                'userAuthentication': {
+                    'emailAddress': 'test-user@example.org',
+                    'password': 'valid-password'
+                }
+            }
+        ),
+        content_type='application/json')
+    assert response_2.status_code == 403
