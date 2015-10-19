c0c30e7df70d3ec9b8d29271b907748629a5737e
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-10-19 15:04:58 +0100

    Changed schedule

--- app/job/job_schedules.py
+++ app/job/job_schedules.py
@@ -4,7 +4,7 @@ from sms_jobs import send_sms
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', minutes=5)
 def send_sms_schedule():
     print("Sending")
     sched.add_job(func=send_sms, max_instances=1, id="sms_sending_job")
