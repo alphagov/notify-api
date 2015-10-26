924b7c0f2bd7a0c0fe67767fd9680a4af7ba90a4
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-10-26 14:05:26 +0000

    Adding usage

--- app/models.py
+++ app/models.py
@@ -83,6 +83,10 @@ class Service(db.Model):
 
     users = db.relationship('User', secondary=user_to_service, backref='services')
 
+    service = db.relationship('Service', backref=db.backref('jobs', lazy='dynamic'))
+
+    usage = db.relationship('Usage', backref=db.backref('usage', lazy='dynamic'))
+
     restricted = db.Column(db.Boolean, index=False, unique=False, nullable=False)
 
     def serialize(self):
@@ -97,6 +101,15 @@ class Service(db.Model):
         }
 
         return filter_null_value_fields(serialized)
+
+
+class Usage(db.Model):
+    __tablename__ = 'usage'
+
+    id = db.Column(db.Integer, primary_key=True)
+    day = db.Column(db.Date, index=False, unique=False, nullable=False)
+    count = db.Column(db.BigInteger, index=False, unique=False, nullable=False)
+    service_id = db.Column(db.Integer, db.ForeignKey('services.id'))
 
 
 class User(db.Model):
